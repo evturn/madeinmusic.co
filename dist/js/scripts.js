a0ea7e3346f5135833b29139ca92332a6f9944c3
Evan Turner <evturn@gmail.com>
2015-08-08 18:32:01 -0400

    add function to read window height and fill landing

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,10 +5,26 @@ var MIM = {
   },
 
   makeFullHeight: function() {
-    $(window).height();
+    var $landing = $('.landing');
+    var $navbar = $('.navbar');
+    var windowHeight = $(window).height();
+    var landingHeight = $landing.outerHeight(true);
+    var navbarHeight = $navbar.outerHeight(true);
+
+    console.log(windowHeight);
+    console.log(landingHeight);
+    console.log(navbarHeight);
+
+    var padding = windowHeight - (landingHeight + navbarHeight);
+    console.log(padding);
+    $('.landing .btn-container').css({'paddingBottom': padding});
   },
 
 };
+
+$(document).on('ready', function() {
+  MIM.init();
+});
 $(function() {
 
   $(window).load(function() {
