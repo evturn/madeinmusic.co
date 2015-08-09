c1f0089f4ba434e87299b6694f0608fd42464edc
Evan Turner <evturn@gmail.com>
2015-08-08 21:27:08 -0400

    updated footer to lock to bottom of window

--- dist/js/es6.js
+++ dist/js/es6.js
@@ -22,20 +22,20 @@ var MIM = {
 $(document).on('ready', function () {
   MIM.init();
 });
-"use strict";
+'use strict';
 
 $(function () {
 
   $(window).load(function () {
-    $("#preloader").delay(500).fadeOut();
-    $(".preloader").delay(600).fadeOut("slow");
+    $('#preloader').delay(500).fadeOut();
+    $('.preloader').delay(600).fadeOut('slow');
   });
 
   $(window).scroll(function () {
     if ($(this).scrollTop() > 1) {
-      $('.scroll-up').css({ bottom: "25px" });
+      $('.scroll-up').css({ bottom: '25px' });
     } else {
-      $('.scroll-up').css({ bottom: "-100px" });
+      $('.scroll-up').css({ bottom: '-100px' });
     }
   });
   $('.scroll-up').click(function () {
