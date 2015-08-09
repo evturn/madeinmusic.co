60d5e4aecc54237dcdd22f54d2f9db2f5129c902
Evan Turner <evturn@gmail.com>
2015-08-08 21:19:52 -0400

    added new scroll-up element

--- dist/js/es6.js
+++ dist/js/es6.js
@@ -33,12 +33,12 @@ $(function () {
 
   $(window).scroll(function () {
     if ($(this).scrollTop() > 1) {
-      $('.dmtop').css({ bottom: "25px" });
+      $('.scroll-up').css({ bottom: "25px" });
     } else {
-      $('.dmtop').css({ bottom: "-100px" });
+      $('.scroll-up').css({ bottom: "-100px" });
     }
   });
-  $('.dmtop').click(function () {
+  $('.scroll-up').click(function () {
     $('html, body').animate({ scrollTop: '0px' }, 800);
     return false;
   });
